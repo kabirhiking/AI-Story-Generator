b3197e1f5340ff3559087e43658208d82fc81d35
kabir <kabirraihan249@gmail.com>
2025-09-22 02:17:39 +0600

    environment variables & configaration , Database setup

---
+++ Backend/models/story.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+
+from ..db.database import Base
+
+
+class Story():
+    pass
